fc22b2eb3fdc2f0d433da5a8a4af078576b6c19d
Shubham <shubhampathade2001@gmail.com>
2024-05-14 23:09:15 +0530

    sdgfbsdh

--- Project/Presentation/Project.Api/obj/Debug/netcoreapp3.1/Project.Api.AssemblyInfo.cs
+++ Project/Presentation/Project.Api/obj/Debug/netcoreapp3.1/Project.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e94c0a1bdea9d1288e2fb7f8823da9e70d1369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b810b20139cfdc18778c87e2257179ebf5e0f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
